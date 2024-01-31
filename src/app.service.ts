6e8493ea9a1ca0ccd9ad080b347054c22233c583
Bao Nghiem <bao.nghiem@clv.local>
2024-01-31 17:32:21 +0700

    test cicd 04

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log("test ci 02")
+    console.log("test ci 0255")
     return 'Hello World!';
   }
 }
